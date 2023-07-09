cdfa0e84c5b4be3a21be05ea43d698d864a0c4b2
Patricia Eiterer <eitererpatricia@gmail.com>
2023-07-09 03:43:43 -0300

    hud basic config

--- src/core/gold/gold-hud-scene.ts
+++ src/core/gold/gold-hud-scene.ts
@@ -3,6 +3,7 @@ import { SUB_GOLD_EVENTS } from './gold-pubsub-events';
 
 export default class HUDScene extends Phaser.Scene {
   private playerCoinTxt: Phaser.GameObjects.Text;
+  private playerTime: Phaser.GameObjects.Text;
 
   constructor() {
     super({
@@ -11,11 +12,27 @@ export default class HUDScene extends Phaser.Scene {
     });
   }
 
+  preload(){
+    this.load.image('coin', 'assets/coin.png');
+    this.load.image('time-icon', 'assets/time-icon.png')
+
+  }
+
   create() {
-    this.playerCoinTxt = this.add.text(50, 10, 'Gold: 0', {
-      color: 'white',
-      fontSize: '28px',
+    let graphics = this.add.graphics();
+    graphics.fillStyle(0xede1ca, 50);
+    graphics.fillRoundedRect(20, 20, 200, 30, 5);
+
+    let coin = this.add.sprite(25, 22, 'coin');
+    coin.setOrigin(0, 0);
+    coin.setDisplaySize(20, 24);
+
+    this.playerCoinTxt = this.add.text(55, 25, '0', {
+      color: '#26150E',
+      fontSize: '24px',
       fontFamily: 'Courier New',
+      stroke: '#26150E',
+      strokeThickness: 2,
     });
 
     this.playerCoinTxt.setOrigin(0);
@@ -27,9 +44,27 @@ export default class HUDScene extends Phaser.Scene {
       this.updateGoldText,
       this
     );
+
+    let timeIcon = this.add.sprite(140, 22, 'time-icon');
+    timeIcon.setOrigin(0, 0);
+    timeIcon.setDisplaySize(24,24);
+
+    this.playerTime = this.add.text(165, 25, '365', {
+      color: '#26150E',
+      fontSize: '24px',
+      fontFamily: 'Courier New',
+      stroke: '#26150E',
+      strokeThickness: 2,
+    });
+
+    this.playerTime.setOrigin(0);
+
+
+  
+  
   }
 
   updateGoldText(gold: number) {
-    this.playerCoinTxt.setText(`Gold: ${gold}`);
-  }
+    this.playerCoinTxt.setText(`${gold}`);
+  }  
 }
